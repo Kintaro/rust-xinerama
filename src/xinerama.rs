cee45af709f88cfc5c477f22a416eea40b9c88d0
Simon Wollwage <mail.wollwage@gmail.com>
2014-11-03 05:06:24 +0100

    added functions

--- src/xinerama.rs
+++ src/xinerama.rs
@@ -3,7 +3,11 @@
 
 use libc::*;
 use xlib::Display;
+use xlib::Window;
+use xlib::Drawable;
+use xlib::XID;
 
+#[repr(C)]
 pub struct XineramaScreenInfo {
    screen_number: c_int,
    x_org : c_short,
@@ -12,8 +16,24 @@ pub struct XineramaScreenInfo {
    height : c_short
 }
 
+#[repr(C)]
+pub struct XPanoramiXInfo {
+    window: Window,
+    screen: c_int,
+    State: c_int,   
+    width: c_int,    
+    height: c_int,    
+    ScreenCount: c_int,
+    eventMask: XID      
+}
+
 #[link(name="Xinerama")]
 extern {
+    pub fn XPanoramiXGetState(dpy: *mut Display, drawable: Drawable, panoramix_info: *mut XPanoramiXInfo) -> c_int;
+    pub fn XPanoramiXGetScreenCount(dpy: *mut Display, drawable: Drawable, panoramix_info: *mut XPanoramiXInfo) -> c_int;
+    pub fn XPanoramiXGetScreenSize(dpy: *mut Display, drawable: Drawable, screen_num: c_int, panoramix_info: *mut XPanoramiXInfo) -> c_int;
+    pub fn XineramaQueryExtension(dpy: *mut Display, event: *mut c_int, error: *mut c_int) -> c_int;
+    pub fn XineramaQueryVersion(dpy: *mut Display, major: *mut c_int, minor: *mut c_int) -> c_int;
 	pub fn XineramaIsActive(dpy: *mut Display) -> c_int;
 	pub fn XineramaQueryScreens(dpy: *mut Display, number: *mut c_int) -> *const XineramaScreenInfo;
 }
